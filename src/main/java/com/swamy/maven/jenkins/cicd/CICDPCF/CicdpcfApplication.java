1a6052f5e8d30945d24dbaa5d8e5273fc5fbf7d9
Swamy Biru <Swamy-Biru@gap.com>
2019-10-16 08:06:51 -0700

    cicd jenkin pipelin PCF

---
+++ src/main/java/com/swamy/maven/jenkins/cicd/CICDPCF/CicdpcfApplication.java
@@ -0,0 +1,13 @@
+package com.swamy.maven.jenkins.cicd.CICDPCF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdpcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdpcfApplication.class, args);
+	}
+
+}
